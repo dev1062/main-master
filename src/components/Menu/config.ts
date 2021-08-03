f4e619ad1559626c59e616be780d67a87b0ebc83
MIR TAUHIDUL ISLAM <mirtauhidulislam@MIRs-MacBook-Air.local>
2021-08-03 10:14:35 -0700

    initial

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,109 @@
+import { MenuEntry } from '@saltswap/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.owld.app/#/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.owld.app/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Salty Pools',
+    icon: 'PoolIcon',
+    href: '/pools',
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  // {
+  //   label: 'Teams & Profile',
+  //   icon: 'GroupsIcon',
+  //   calloutClass: 'rainbow',
+  //   items: [
+  //     {
+  //       label: 'Leaderboard',
+  //       href: '/teams',
+  //     },
+  //     {
+  //       label: 'Task Center',
+  //       href: '/profile/tasks',
+  //     },
+  //     {
+  //       label: 'Your Profile',
+  //       href: '/profile',
+  //     },
+  //   ],
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'Overview',
+        href: 'https://saltswap.info',
+      },
+      {
+        label: 'Tokens',
+        href: 'https://saltswap.info/tokens',
+      },
+      {
+        label: 'Pairs',
+        href: 'https://saltswap.info/pairs',
+      },
+      {
+        label: 'Accounts',
+        href: 'https://saltswap.info/accounts',
+      },
+    ],
+  },
+  // {
+  //   label: 'IFO',
+  //   icon: 'IfoIcon',
+  //   href: '/ifo',
+  // },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/owldefi',
+      },
+      {
+        label: 'Docs',
+        href: 'https://owld.gitbook.io/docs/',
+      },
+      {
+        label: 'Blog',
+        href: 'https://medium.com/@owldefi',
+      },
+    ],
+  },
+]
+
+export default config
